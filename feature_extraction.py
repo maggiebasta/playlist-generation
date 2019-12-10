28658621b0cd98e68064afa4bdd59ccf52ac4eff
Marguerite Basta <mbasta@pixability.com>
2019-12-10 13:49:04 -0500

    feature extraction updates

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,9 +1,16 @@
 import itertools
+import sys
 
 import pandas as pd
 import numpy as np
 
 from spotify_api_database import SpotifyAuth
+
+sys.path.append('Models')
+import wrmf_helpers
+from settings import PATH_TO_SPARSE_MATRIX
+
+_, tid_to_idx, _, _, _ = wrmf_helpers.get_user_item_sparse_matrix(PATH_TO_SPARSE_MATRIX)
 
 
 def get_song_features(tid):
@@ -92,25 +99,28 @@ def compute_seedset_features(tids):
     return summary_feats
 
 
-def get_X_train(seed_tids, candidate_tids):
+def compute_df_features(seed_tids, candidate_tids, relevences):
     """
     Given a list of seed track ids and candidate_tids returns a
     dataframe summarizing the differences between each candidate
-    track's features and seed features. Note, Xtrain should have
-    nulls handled
-    :seed_tids: seed track ids
-    :returns: candidate_tids candidate track ids
+    track's features and seed features.
+    :param seed_tids: seed track ids
+    :param candidate_tids: candidate track ids
+    :param relevences: list of relevence for each candidate
     """
     seed_features = compute_seedset_features(seed_tids)
 
     # drop candidate songs w/0 all features
-    candidate_df = get_features_dataframe(candidate_tids).dropna(axis=0)
-    Xtrain = {}
-    Xtrain['artist_overlap'] = [
+    candidate_df = get_features_dataframe(candidate_tids)
+    candidate_df['relevence'] = relevences
+    candidate_df.dropna(axis=0)
+    df = {}
+    df['relevence'] = candidate_df['relevence']
+    df['artist_overlap'] = [
         1 if len(np.intersect1d(x, seed_features['artists'])) else 0
         for x in candidate_df['artists']
     ]
-    Xtrain['album_overlap'] = [
+    df['album_overlap'] = [
         1 if x in seed_features['albums'] else 0
         for x in candidate_df['album']
     ]
@@ -122,7 +132,47 @@ def get_X_train(seed_tids, candidate_tids):
     ]
     for feat in numeric:
         diff = (candidate_df[feat] - seed_features[feat]).abs()
-        Xtrain[f'{feat}_diff'] = diff
+        df[f'{feat}_diff'] = diff
 
-    Xtrain = pd.DataFrame.from_dict(Xtrain)
-    return Xtrain, seed_features, candidate_df
+    df = pd.DataFrame.from_dict(df)
+    return df
+
+
+def compute_df(playlist, song_factors, playlist_factors):
+    """
+    Given an input playlist and factors computed from stage 1,
+    returns a df for stage 2
+
+    :param playlist: list of song ids making up the playlist
+    :param song_factors: song factors from wrmf
+    :param playlist_factors: playlist factors from wrmf
+    """
+    playlist = playlist.str.replace('spotify:track:', '')
+    playlist_set = set(playlist)
+    seed_ids = []
+    while len(seed_ids) < 2:
+        rand = list(playlist.sample(n=1))[0]
+        if rand in tid_to_idx and rand not in seed_ids:
+            seed_ids.append(rand)
+    playlist_set.remove(seed_ids[0])
+    playlist_set.remove(seed_ids[1])
+    wrmf_ensemble_output = wrmf_helpers.get_top_similar_from_ensemble(
+            song_factors,
+            playlist_factors,
+            seed_ids,
+            n_similar_songs=20000,
+            n_similar_playlists=200
+    )
+    wrmf_ensemble_output_set = set(wrmf_ensemble_output)
+    true_matches = playlist_set.intersection(wrmf_ensemble_output_set)
+    false_matches = wrmf_ensemble_output_set.symmetric_difference(true_matches)
+
+    X_train_ids = []
+    Y_train = []
+    for _ in range(min(len(true_matches), 20)):
+        X_train_ids.append(true_matches.pop())
+        Y_train.append(1)
+        X_train_ids.append(false_matches.pop())
+        Y_train.append(0)
+
+    return compute_df_features(seed_ids, X_train_ids, Y_train)
